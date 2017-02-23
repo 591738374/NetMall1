58990fd849ce40ee45881982951b01da5314e0cb
GodFavoredOne <1076739869@qq.com>
2017-02-23 14:19:59 +0800

    首页布局实现

--- app/src/main/java/com/example/netmall/home/fragment/HomeFragment.java
+++ app/src/main/java/com/example/netmall/home/fragment/HomeFragment.java
@@ -1,11 +1,17 @@
 package com.example.netmall.home.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.netmall.R;
 import com.example.netmall.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by WZ on 2017/2/22.
@@ -13,19 +19,48 @@ import com.example.netmall.base.BaseFragment;
  */
 
 public class HomeFragment extends BaseFragment {
-    private TextView textView;
+    @InjectView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @InjectView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @InjectView(R.id.rv_home)
+    RecyclerView rvHome;
+    @InjectView(R.id.ib_top)
+    ImageButton ibTop;
+
     @Override
     public View initView() {
-        textView=new TextView(mContext);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("主页fragment数据初始化");
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(mContext, "进入消息中心", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_top:
+//                Toast.makeText(mContext, "置顶", Toast.LENGTH_SHORT).show();
+                rvHome.scrollToPosition(0);
+                break;
+        }
     }
 }
